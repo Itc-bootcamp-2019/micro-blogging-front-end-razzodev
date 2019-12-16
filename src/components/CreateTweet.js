f27288e71c62fa45a1db302a59462c1039f3576a
Raz Nir <razzo.des@gmail.com>
2019-12-16 19:08:34 +0200

    reformat: rm states, set props

--- src/components/CreateTweet.js
+++ src/components/CreateTweet.js
@@ -1,42 +1,42 @@
-import React, { useState } from 'react';
+import React, {  } from 'react';
 import './CreateTweetStyle.css';
 
 
 function CreateTweet(props) {
 
-    const [maxChar, setMaxChar] = useState(false);
-    const maxCharCheck = () => {
-        return (tweetContent.length > 140) ? setMaxChar(true) : setMaxChar(false);
-    }
-    const [ isValid , setIsValid ] = useState(false);
-    const checkValid = () => {
-        return (tweetContent.length > 0 && !maxChar) ? setIsValid(true) : setIsValid(false);
-    };
+    // const [maxChar, setMaxChar] = useState(false);
+    // const maxCharCheck = () => {
+    //     return (tweetContent.length > 140) ? setMaxChar(true) : setMaxChar(false);
+    // }
+    // const [ isValid , setIsValid ] = useState(false);
+    // const checkValid = () => {
+    //     return (tweetContent.length > 0 && !maxChar) ? setIsValid(true) : setIsValid(false);
+    // };
     
-    const [tweetContent, setTweetContent] = useState('');
-    const tweetInput = (e) => {
-        setTweetContent(e.target.value);
-        maxCharCheck();
-        checkValid();
-    }
+    // const [tweetContent, setTweetContent] = useState('');
+    // const tweetInput = (e) => {
+    //     setTweetContent(e.target.value);
+    //     maxCharCheck();
+    //     checkValid();
+    // }
     
-    const [ currentTweet, setCurrentTweet] = useState({});
-    const submitTweet = () => {
-        return setCurrentTweet({
-            username:'raz',
-            timeCreated: new Date().toISOString(),
-            content: tweetContent,
-        });
-        // console.log(currentTweet); // FIRST CLICK RETURNS EMPTY OBJECT --ASYNC(?)
-    }
-
+    // const [ currentTweet, setCurrentTweet] = useState({});
+    // const submitTweet = () => {
+    //     return setCurrentTweet({
+    //         username:'raz',
+    //         timeCreated: new Date().toISOString(),
+    //         content: tweetContent,
+    //     });
+    //     // console.log(currentTweet); // FIRST CLICK RETURNS EMPTY OBJECT --ASYNC(?)
+    // }
+    const { maxChar , input , disabled , submit } = props;
     return (
         <div className='create-tweet-wrapper'>
             <textarea id='tweetInput' placeholder='What you have in mind...'
-                onChange={(e) => tweetInput(e)} />
+                onChange={input} />
             <div>
                 {maxChar && <div id='maxCharWarning'>The tweet can't contain more than 140 chars.</div>}
-                <button id='tweetButton' disabled={!isValid} onClick={() => submitTweet()} >Tweet</button>
+                <button id='tweetButton' disabled={disabled} onClick={submit} >Tweet</button>
             </div>
         </div>
     )
