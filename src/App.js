64f49ac099dc8d6699d7a9fe76dc81965a2aca85
Raz Nir <razzo.des@gmail.com>
2019-12-16 15:02:56 +0200

    add Tweet comp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import CreateTweet from './components/CreateTweet';
+import Tweet from './components/Tweet';
 
 function App() {
   return (
     <div className="App">
       <CreateTweet  />
-
+      <Tweet username={'raz'}
+      timeCreated={new Date().toISOString()}
+      content={'foobar!'}/>
     </div>
   );
 }
