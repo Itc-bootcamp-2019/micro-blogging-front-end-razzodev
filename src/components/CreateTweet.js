63ebc18c06af9f5a20fbb4579119faf188835dc6
Raz Nir <razzo.des@gmail.com>
2019-12-16 16:39:00 +0200

    create object from input, btn disable condition

--- src/components/CreateTweet.js
+++ src/components/CreateTweet.js
@@ -3,26 +3,41 @@ import './CreateTweetStyle.css';
 
 
 function CreateTweet(props) {
-    const [ tweet , setTweet ] = useState('');
-    const [ maxChar , setMaxChar ] = useState(false);
-    
-    const tweetInput = (e) => {
-        setTweet(e.target.value);
-        maxCharCheck();
-    }
 
+    const [maxChar, setMaxChar] = useState(false);
     const maxCharCheck = () => {
-        return (tweet.length > 140) ? setMaxChar(true) : setMaxChar(false);
+        return (tweetContent.length > 140) ? setMaxChar(true) : setMaxChar(false);
+    }
+    const [ isValid , setIsValid ] = useState(false);
+    const checkValid = () => {
+        (tweetContent.length > 0 && !maxChar) ? setIsValid(true) : setIsValid(false);
+    };
+    
+    const [tweetContent, setTweetContent] = useState('');
+    const tweetInput = (e) => {
+        setTweetContent(e.target.value);
+        maxCharCheck();
+        checkValid();
+    }
+    
+    const [ currentTweet, setCurrentTweet] = useState({});
+
+    const submitTweet = () => {
+        setCurrentTweet({
+            username:'raz',
+            timeCreated: new Date().toISOString(),
+            content: tweetContent,
+        });
+        console.log(currentTweet); // FIRST CLICK RETURNS EMPTY OBJECT --ASYNC(?)
     }
 
     return (
         <div className='create-tweet-wrapper'>
-            <textarea id='tweetInput'
-                placeholder='What you have in mind...'
+            <textarea id='tweetInput' placeholder='What you have in mind...'
                 onChange={(e) => tweetInput(e)} />
-            <div> 
+            <div>
                 {maxChar && <div id='maxCharWarning'>The tweet can't contain more than 140 chars.</div>}
-                <button id='tweetButton' >Tweet</button>
+                <button id='tweetButton' disabled={!isValid} onClick={() => submitTweet()} >Tweet</button>
             </div>
         </div>
     )
