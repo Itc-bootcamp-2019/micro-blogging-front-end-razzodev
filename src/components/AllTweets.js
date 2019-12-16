b220cda69232e38e5e1e81a04ba5e1a0e02b90dd
Raz Nir <razzo.des@gmail.com>
2019-12-16 15:03:10 +0200

    initial commit

---
+++ src/components/AllTweets.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Tweet from "./Tweet";
+function AllTweets(props) {
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AllTweets
