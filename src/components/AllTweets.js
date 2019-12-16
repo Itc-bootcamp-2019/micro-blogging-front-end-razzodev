7cbd38f7aed0ce31afde3699bdda0c331f67ef3e
Raz Nir <razzo.des@gmail.com>
2019-12-16 16:37:10 +0200

    prepend new tweet to array, create Tweet comps

--- src/components/AllTweets.js
+++ src/components/AllTweets.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState , useEffect } from 'react';
 import Tweet from "./Tweet";
+
+
+
 function AllTweets(props) {
-    
+    const { newTweet } = props;
+    const [ tweets , setTweets ] = useState([]);
+
+    useEffect(() => {
+        setTweets([newTweet, ...tweets ])
+    },[newTweet,tweets]);
+
     return (
-        <div>
-            
+        <div /*style={{marginTop: '20px'}}*/>
+            {tweets.map(tweet => 
+                <Tweet username={tweet.username} timeCreated={tweet.timeCreated} content={tweet.content} />
+                )}
         </div>
     )
 }
 
 export default AllTweets
+
