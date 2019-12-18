0074d997726cbe4b04a18901143ca67f14ea7961
Raz Nir <razzo.des@gmail.com>
2019-12-18 20:05:41 +0200

    refactor: move states to homepage

--- src/App.js
+++ src/App.js
@@ -1,41 +1,26 @@
+import React from 'react';
 import './App.css';
-import React, { useState , useEffect } from 'react';
-import { getTweets , postTweets } from './lib/api'
-import CreateTweet from './components/CreateTweet';
-import AllTweets from './components/AllTweets';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Profile from './pages/Profile'
+import Navbar from './components/Navbar';
 
 function App(props) {
-  
-  const [ loading , setLoading ] = useState(true);
-  const [ tweets , setTweets ] = useState([]);
-  
-  const submitTweet = (tweetContent) => {
-      let newTweet = {
-        content: tweetContent,
-        date: new Date().toISOString(),
-        userName: 'aba',
-      }
-      try{
-        postTweets(newTweet);
-        getTweets().then(data => setTweets(data))
-      } catch (error) {
-        alert(error);
-      }
-  }; 
-
-  useEffect(() => {
-    setInterval(() => 
-    getTweets().then(data => setTweets(data)).then(() => setLoading(false))
-    , 5000)
-  },[]);
-
   return (
     <div className="App">
-      <CreateTweet submit={(e) => submitTweet(e)} />
-      {loading && <h3>Loading...</h3>}
-      <AllTweets list={tweets} />
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route exact path='/profile'>
+            <Profile />
+          </Route>
+        </Switch>
+      </Router>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import { getTweets, postTweets } from '../lib/api'
+import CreateTweet from '../components/CreateTweet';
+import AllTweets from '../components/AllTweets';
+
+function Home() {
+    const [loading, setLoading] = useState(true);
+    const [tweets, setTweets] = useState([]);
+
+    const submitTweet = (tweetContent) => {
+        let newTweet = {
+            content: tweetContent,
+            date: new Date().toISOString(),
+            userName: 'aba',
+        }
+        try {
+            postTweets(newTweet);
+            getTweets().then(data => setTweets(data))
+        } catch (error) {
+            alert(error);
+        }
+    };
+
+    useEffect(() => {
+        setInterval(() =>
+            getTweets().then(data => setTweets(data)).then(() => setLoading(false))
+            , 5000)
+    }, []);
+    return (
+        <div>
+            <CreateTweet submit={(e) => submitTweet(e)} />
+            {loading && <h3>Loading...</h3>}
+            <AllTweets list={tweets} />
+        </div>
+    )
+}
+
+export default Home
