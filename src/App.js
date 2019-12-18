00cb40d2d3e17677c2ea3bca37bfa5436d37144d
Raz Nir <razzo.des@gmail.com>
2019-12-18 15:37:41 +0200

    feat: load data from local storage

--- src/App.js
+++ src/App.js
@@ -1,40 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 import './App.css';
 import CreateTweet from './components/CreateTweet';
-import Tweet from './components/Tweet';
 import AllTweets from './components/AllTweets';
 
 function App(props) {
-  const [maxChar, setMaxChar] = useState(false);
-  const maxCharCheck = () => {
-      return (tweetContent.length > 140) ? setMaxChar(true) : setMaxChar(false);
-  }
-
-  const [ isValid , setIsValid ] = useState(false);
-  const checkValid = () => {
-      return (tweetContent.length > 0 && !maxChar) ? setIsValid(true) : setIsValid(false);
-  };
+  const inStorage = localStorage.getItem('tweets');
+  const [ tweets , setTweets ] = useState(
+    JSON.parse(inStorage) || []
+  );
   
-  const [tweetContent, setTweetContent] = useState('');
-  const tweetInput = (e) => {
-      setTweetContent(e.target.value);
-      maxCharCheck();
-      checkValid();
-  }
-  
-  const [ tweets , setTweets ] = useState([]);
-
-  const submitTweet = () => {
+  const submitTweet = (tweetContent) => {
       let newTweet = {
         content: tweetContent,
+        date: new Date().toISOString(),
         userName: 'raz',
-        date: new Date().toISOString()
       }
       setTweets([ newTweet , ...tweets]);
   };
+
+  useEffect(() => {
+    localStorage.setItem('tweets', JSON.stringify(tweets)); 
+  },[tweets]);
+
   return (
     <div className="App">
-      <CreateTweet maxChar={maxChar} disabled={!isValid} input={(e) => tweetInput(e)} submit={() => submitTweet()}  />
+      <CreateTweet submit={(e) => submitTweet(e)} />
       <AllTweets list={tweets} />
     </div>
   );
