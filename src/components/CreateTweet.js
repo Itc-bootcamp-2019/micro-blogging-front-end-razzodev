d7a091060633102c45af946943bb76c3a407d0fc
Raz Nir <razzo.des@gmail.com>
2019-12-18 15:39:14 +0200

    refactor: child states

--- src/components/CreateTweet.js
+++ src/components/CreateTweet.js
@@ -1,17 +1,34 @@
-import React, {  } from 'react';
+import React, { useState } from 'react';
 import './CreateTweetStyle.css';
 
 
 function CreateTweet(props) {
-    const { maxChar , input , disabled , submit } = props;
+    const { submit } = props;
+
+    const [maxChar, setMaxChar] = useState(false); // limit tweet to 140 characters
+    const maxCharCheck = () => {
+        return (tweetContent.length > 140) ? setMaxChar(true) : setMaxChar(false);
+    }
+  
+    const [ isValid , setIsValid ] = useState(false); // enable-disable submit button
+    const checkValid = () => {
+        return (tweetContent.length > 0 && !maxChar) ? setIsValid(true) : setIsValid(false);
+    };
+    
+    const [tweetContent, setTweetContent] = useState(''); 
+    const tweetInput = (e) => {
+        setTweetContent(e.target.value);
+        maxCharCheck();
+        checkValid();
+    }
 
     return (
         <div className='create-tweet-wrapper'>
             <textarea id='tweetInput' placeholder='What you have in mind...'
-                onChange={input} />
+                onChange={(e) => tweetInput(e)} />
             <div>
                 {maxChar && <div id='maxCharWarning'>The tweet can't contain more than 140 chars.</div>}
-                <button id='tweetButton' disabled={disabled} onClick={submit} >Tweet</button>
+                <button id='tweetButton' disabled={!isValid} onClick={() => submit(tweetContent)} >Tweet</button>
             </div>
         </div>
     )
