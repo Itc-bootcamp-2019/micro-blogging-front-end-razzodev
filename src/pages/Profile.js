1258b58dab814e52757141d5f8a3f87126ab658c
Raz Nir <razzo.des@gmail.com>
2019-12-21 19:58:57 +0200

    event handler set username

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState, useContext } from 'react';
+import { UserNameContext } from '../context'
 
 function Profile() {
+    const { name , setName } = useContext(UserNameContext);
+    const submitName = () => {
+        console.log(name);
+    }
     return (
         <div>
-            
+            <h3 style={{ color: '#FFFFFF' }}>{name}</h3>
+                <input placeholder='username' onChange={(e) =>
+                    setName(e.target.value)} />
+                <button onClick={() => submitName()}>Confirm</button>
         </div>
     )
 }
