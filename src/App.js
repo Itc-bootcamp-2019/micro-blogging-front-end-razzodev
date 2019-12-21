df4853c242e805a96b6e8e299e5e6400d01c6775
Raz Nir <razzo.des@gmail.com>
2019-12-21 19:56:25 +0200

    add context provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { UserNameContext } from './context'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Profile from './pages/Profile'
 import Navbar from './components/Navbar';
 
 function App(props) {
+  const [ name , setName ] = useState('Guest');
   return (
     <div className="App">
       <Router>
@@ -15,7 +17,9 @@ function App(props) {
             <Home />
           </Route>
           <Route exact path='/profile'>
+            <UserNameContext.Provider value={name}>
             <Profile />
+            </UserNameContext.Provider>
           </Route>
         </Switch>
       </Router>
