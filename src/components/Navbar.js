dede7bdcb9f43db4270ee8efaf95f1df67875a6d
Raz Nir <razzo.des@gmail.com>
2019-12-21 23:20:18 +0200

    update username

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,21 @@
-import React , { useContext } from 'react'
-import { BrowserRouter as Router , NavLink } from "react-router-dom";
+import React , { useContext, useEffect, useState } from 'react'
+import { NavLink } from "react-router-dom";
 import { UserNameContext } from '../context';
 import '../components/NavbarStyle.css';
 
 function Navbar() {
-    const {name} = useContext(UserNameContext);
+    const { userName , setUsername } = useContext(UserNameContext);
+
     return (
         <div id='navbar'>
             <NavLink exact activeClassName='active-links' className='nav-links' to='/'>Home</NavLink>
             <NavLink exact activeClassName='active-links' className='nav-links' to='/profile'>Profile</NavLink>
             <h5 style={{
+
                 paddingLeft: '40px',
                 color: '#FFFFFF',
                 fontWeight: '500'
-                }}>{name}</h5>
+                }}>Hi, {userName}</h5>
         </div>
     )
 }
