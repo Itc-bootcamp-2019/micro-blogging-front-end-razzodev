66f049f7a27ef192b509de4997d38c3fede4ec96
Raz <razzo.des@gmail.com>
2019-12-24 15:17:24 +0200

    add tweets to firebase collection

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useContext } from "react";
-import { getTweets, postTweets } from "../lib/api";
 import { UserNameContext } from "../context";
 import CreateTweet from "../components/CreateTweet";
 import AllTweets from "../components/AllTweets";
@@ -7,32 +6,28 @@ import { dbCollection } from "../lib/firebaseFunc";
 
 function Home() {
   const [loading, setLoading] = useState(true);
-  const [tweets, setTweets] = useState([]);
   const userName = useContext(UserNameContext);
-  const [tweetsFB, setTweetsFB] = useState([]);
+  const [tweetsFirebase, setTweetsFirebase] = useState([]);
   let tempArray = [];
+
   const readFromCloud = async () => {
     try {
       const get = await dbCollection.get();
       await get.forEach(doc => tempArray.push(doc.data()));
-      setTweetsFB(tempArray);
-      console.log("executed");
+      await tempArray.reverse();
+      setTweetsFirebase(tempArray);
     } catch (error) {
-      console.error(error + "THE RROR");
+      console.error("Error when trying to fetch data from firebase:");
+      console.error(error);
     }
   };
 
-  useEffect(() => {
-    setInterval(() => readFromCloud().then(() => setLoading(false)), 3000);
-    // setInterval(
-    //   () =>
-    //     getTweets()
-    //       .then(data => setTweets(data))
-    //       .then(() => setLoading(false)),
-    //   5000
-    // );
-    return () => {};
-  }, []);
+  const sendToCloud = document => {
+    dbCollection
+      .doc(`${document.date}:${document.userName}`)
+      .set(document)
+      .then(readFromCloud());
+  };
 
   const submitTweet = (tweetContent, name) => {
     let nameConvert = JSON.stringify(name);
@@ -43,21 +38,22 @@ function Home() {
       userName: nameSlice
     };
     try {
-      postTweets(newTweet);
-      getTweets().then(data => setTweets(data));
+      sendToCloud(newTweet);
     } catch (error) {
       alert(error);
     }
   };
 
+  useEffect(() => {
+    readFromCloud().then(() => setLoading(false));
+    return () => {};
+  }, []);
+
   return (
     <div>
       <CreateTweet submit={e => submitTweet(e, userName)} />
       {loading && <h3>Loading...</h3>}
-      <h4>DB Collection</h4>
-      <AllTweets list={tweetsFB} />
-      <h4>Class Server</h4>
-      <AllTweets list={tweets} />
+      <AllTweets list={tweetsFirebase} />
     </div>
   );
 }
